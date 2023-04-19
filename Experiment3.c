b0f00b3c37f241821e797508e32726355ac5a91e
idiallo-su <113380366+idiallo-su@users.noreply.github.com>
2023-04-19 11:44:55 -0400

    Create Experiment3.c

---
+++ Experiment3.c
@@ -0,0 +1,316 @@
+/*
+ * File:   newmain.c
+ * Author: ngguaste
+ *
+ * Created on September 15, 2021, 4:58 PM
+ */
+ 
+ 
+#include <xc.h>
+ 
+void main(void)
+{ 
+	PORT_REGS->GROUP[0].PORT_DIRSET = 0b00000000000000000000101111111100;  // sets pins to an output 00 000 000 000 000 000 000 111 111 101 100
+	PORT_REGS->GROUP[0].PORT_DIRCLR = 0b00000000000000010000000000000000;  // Pushbutton  for increment set as input on PA16, physical pin 17
+	PORT_REGS->GROUP[0].PORT_DIRCLR = 0b00000000000001000000000000000000;  // Pushbutton  for direction set as input on PA18, physical pin 19
+	PORT_REGS->GROUP[0].PORT_DIRCLR = 0b00000000010000000000000000000000;  // Pushbutton  for reset 	set as input on PA22, physical pin 21
+	PORT_REGS->GROUP[0].PORT_PINCFG[16] = 0b00000010;                  	// Input enable for PA16, physical pin 17
+	PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0b00000010;                  	// Input enable for PA18, physical pin 19
+	PORT_REGS->GROUP[0].PORT_PINCFG[22] = 0b00000010;                  	// Input enable for PA22, physical pin 21
+	
+	int tenCount = 0;
+	int oneCount = 0;
+	int flagInc = 1;
+	int flagDirection = 1;
+	int flagReset = 1;
+	int direction = 0; //0 for forward, 1 for reverse
+	
+	while(1)
+	{
+    	//Code for changing direction
+    	if((PORT_REGS->GROUP[0].PORT_IN & 0b00000000000001000000000000000000) == 0b00000000000001000000000000000000)
+    	{
+        	if(flagDirection == 1)
+        	{
+            	if(direction == 1)
+            	{
+                	direction = 0;
+                	flagDirection = 0;
+            	}
+            	else if(direction == 0)
+            	{
+                	direction = 1;
+                	flagDirection = 0;
+            	}
+        	}
+    	}
+    	else
+    	{
+        	flagDirection = 1;
+    	}
+    	
+    	//Code for reset button
+    	if((PORT_REGS->GROUP[0].PORT_IN & 0b00000000010000000000000000000000) == 0b00000000010000000000000000000000)
+    	{
+        	if(flagReset == 1)
+        	{
+                	tenCount = 0;
+                	oneCount = 0;
+                	flagReset = 0;
+        	}
+    	}
+    	else
+    	{
+        	flagReset = 1;
+    	}
+    	
+    	//Code for increment or decrementing the count
+    	if ((PORT_REGS->GROUP[0].PORT_IN & 0b00000000000000010000000000000000) == 0b00000000000000010000000000000000)
+    	{
+       	if (flagInc == 1)
+       	{
+           	if(direction == 0) // forward/counting up
+           	{
+               	if (oneCount < 9)
+               	{
+                   	oneCount = oneCount + 1;
+                   	flagInc = 0;
+               	}
+               	else if ((tenCount == 9)&&(oneCount == 9))
+               	{
+                   	tenCount = 0;
+                   	oneCount = 0;
+                   	flagInc = 0;
+               	}
+               	else if (oneCount == 9)
+               	{
+                   	oneCount = 0;
+                   	tenCount = tenCount + 1;
+                   	flagInc = 0;
+               	}
+           	}
+           	else if(direction == 1) // backwards/counting down
+           	{
+               	if (oneCount > 0)
+               	{
+                   	oneCount = oneCount - 1;
+                   	flagInc = 0;
+               	}
+               	else if ((tenCount == 0)&&(oneCount == 0))
+               	{
+                   	tenCount = 9;
+                   	oneCount = 9;
+                   	flagInc = 0;
+               	}
+               	else if (oneCount == 0)
+               	{
+                   	oneCount = 9;
+                   	tenCount = tenCount - 1;
+                   	flagInc = 0;
+               	}
+           	}
+       	}
+    	}  
+    	else
+    	{
+        	flagInc = 1;
+    	}  
+ 	
+    	//Switch case for the ones digit (second display)
+    	switch(oneCount)
+    	{
+        	case 0:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000011111100; //  Output the number 0
+            	delay(1000);
+            	break;
+ 
+        	case 1:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000000011000; //  Output the number 1
+            	delay(1000);
+            	break;
+ 
+        	case 2:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001001101100; //  Output the number 2
+            	delay(1000);
+            	break;
+ 
+        	case 3:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001000111100; //  Output the number 3
+            	delay(1000);
+            	break;
+ 
+        	case 4:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010011000; //  Output the number 4
+            	delay(1000);    	
+            	break;
+ 
+        	case 5:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010110100; //  Output the number 5
+            	delay(1000);    	
+            	break;
+ 
+        	case 6:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001011110100; //  Output the number 6
+            	delay(1000);    	
+            	break;
+ 
+        	case 7:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000000011100; //  Output the number 7
+            	delay(1000);    	
+            	break;
+ 
+        	case 8:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001011111100; //  Output the number 8
+            	delay(1000);    	
+            	break;
+ 
+        	case 9:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000100000000; //turn off first display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000100000000000; //turn on second display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010111100; //  Output the number 9
+            	delay(1000);    	
+            	break;
+    	}  
+ 
+    	//Switch case for the tens digit (first display)
+    	switch(tenCount)
+    	{
+        	case 0:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000011111100; //  Output the number 0
+            	delay(1000);    	
+            	break;
+ 
+        	case 1:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000000011000; //  Output the number 1
+            	delay(1000);    	
+            	break;
+ 
+        	case 2:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001001101100; //  Output the number 2
+            	delay(1000);    	
+            	break;
+ 
+        	case 3:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001000111100; //  Output the number 3
+            	delay(1000);    	
+            	break;
+ 
+        	case 4:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010011000; //  Output the number 4
+            	delay(1000);    	
+            	break;
+ 
+        	case 5:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010110100; //  Output the number 5
+            	delay(1000);    	
+            	break;
+ 
+        	case 6:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001011110100; //  Output the number 6
+            	delay(1000);    	
+            	break;
+ 
+        	case 7:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000000011100; //  Output the number 7
+            	delay(1000);    	
+            	break;
+ 
+        	case 8:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001011111100; //  Output the number 8
+            	delay(1000);    	
+            	break;
+ 
+        	case 9:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010111100; //  Output the number 9
+            	delay(1000);    	
+            	break;
+            	
+        	default:
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000100000000000; //turn off second display
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //global clear
+                PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000000100000000; //turns on first display
+                PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000011111100; //  Output the number 0
+            	delay(1000);
+    	}  
+	}
+	return;
+}
+ 
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000011111100; //  Output the number 0
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000000011000; //  Output the number 1
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001001101100; //  Output the number 2
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001000111100; //  Output the number 3
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010011000; //  Output the number 4
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010110100; //  Output the number 5
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001011110100; //  Output the number 6
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000000000011100; //  Output the number 7
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001011111100; //  Output the number 8
+//PORT_REGS->GROUP[0].PORT_OUTSET = 0b00000000000000000000001010111100; //  Output the number 9
+//PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //clear first display
+//PORT_REGS->GROUP[0].PORT_OUTCLR = 0b00000000000000000000001011111100; //clear second display
+ 
+void delay(unsigned int m)
+{
+unsigned int i;
+for(i=0; i<=m; i++)
+           {
+}
+}
